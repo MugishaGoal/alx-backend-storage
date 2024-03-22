f51356f7c3c5efc9e72d3d24f9aa0347a20c8c47
Mugisha Goal <degaullemugisha@gmail.com>
2024-03-22 11:09:48 +0000

    List all bands with Glam rock as their main style

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,11 +1,6 @@
 -- List all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT 
-    band_name,
-    IF(split IS NULL, 2022 - formed, 2022 - split) AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
