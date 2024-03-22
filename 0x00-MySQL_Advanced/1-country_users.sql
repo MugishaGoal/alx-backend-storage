ffaa1cdcbe55c9e3381116041c2b69a253826d1b
Mugisha Goal <degaullemugisha@gmail.com>
2024-03-22 10:03:33 +0000

    Create table users with specified attributes

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Create table users with specified attributes including an enumeration for country
+
+-- CREATE TABLE query to create table users
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
