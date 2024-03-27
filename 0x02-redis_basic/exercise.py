3f6951ed27ee4fc886d28c54759b971b4cf7cd90
Mugisha Goal <degaullemugisha@gmail.com>
2024-03-27 21:37:22 +0000

    Cache class that interacts with Redis.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,3 +19,29 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieve data from Redis using the provided key and optionally apply a conversion function.
+
+        Args:
+            key: The key used to retrieve the data from Redis.
+            fn: Optional conversion function to apply to the retrieved data.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data, optionally converted based on the provided function.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """Retrieve string data from Redis using the provided key"""
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """Retrieve integer data from Redis using the provided key"""
+        return self.get(key, fn=int)
