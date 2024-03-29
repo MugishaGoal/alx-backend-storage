c33c79157066ac8a46dd1b352c425987d0d85be3
Mugisha Goal <degaullemugisha@gmail.com>
2024-03-29 23:49:18 +0000

    Python scripts for MongoDB manipulation

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
-"""Script to provide stats about Nginx logs stored in MongoDB"""
-
-
+'''Task 12's module.
+'''
 from pymongo import MongoClient
 
 
 def nginx_log_request(nginx_collection):
-    """Prints stats about Nginx request logs"""
+    '''Prints stats about Nginx request logs.
+    '''
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
+        request_count = len(list(nginx_collection.find({'method': method})))
+        print('\method {}: {}'.format(method, request_count))
     status_checks_count = len(list(
         nginx_collection.find({'method': 'GET', 'path': '/status'})
     ))
     print('{} status check'.format(status_checks_count))
 
 
-def start():
-    """Main function"""
+def run():
+    '''Provides some stats about Nginx logs stored in MongoDB.
+    '''
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_log_request(client.logs.nginx)
 
 
 if __name__ == '__main__':
-    start()
+    run()
